2433b5674b026df3535de90fa4a450f0a682b16c
Netanel Lila <427netanel@gmail.com>
2023-06-08 13:07:12 +0300

    CLientA

--- src/main/java/com/example/clienapi/ClienApiApplication.java
+++ src/main/java/com/example/clienapi/ClienApiApplication.java
@@ -2,12 +2,51 @@ package com.example.clienapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+import java.io.FileOutputStream;
+import  java.io.IOException;
+import  java.net.http.*;
+import  java.net.URI;
+import java.io.File;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+
 public class ClienApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClienApiApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException, InterruptedException {
 
+        //first (time)
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/time")).GET().build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        String apiResponse = response.body();
+        System.out.println(apiResponse);
+
+        //second (json)
+        HttpRequest requetip = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/json")).GET().build();
+        HttpResponse<String> ip = httpClient.send(requetip, HttpResponse.BodyHandlers.ofString());
+        String apipp = ip.body();
+        System.out.println(apipp);
+
+
+        //third (image)
+        String path="http://localhost:8080/api/image";
+        HttpClient client=HttpClient.newHttpClient();
+        try {
+
+            HttpRequest httpRequest=HttpRequest.newBuilder()
+                    .uri(new URI(path))
+                    .header("Content-Type","application/json")
+                    .GET().build();
+            HttpResponse<byte[]> reque=client.send(httpRequest,HttpResponse.BodyHandlers.ofByteArray());
+            System.out.println(reque.body().length);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
--- src/test/java/com/example/clienapi/ClienApiApplicationTests.java
+++ src/test/java/com/example/clienapi/ClienApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ClienApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
