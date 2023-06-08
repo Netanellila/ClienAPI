a5efa54403468ea06ab163da10a12491adf89b37
Netanel Lila <427netanel@gmail.com>
2023-06-08 13:06:05 +0300

    Initial commit

---
+++ src/test/java/com/example/clienapi/ClienApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clienapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
